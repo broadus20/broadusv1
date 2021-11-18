67a060d705e6d26f03e89d23f8e7dbc43f8ddfcf
broadus20 <stephenbroadus197@gmail.com>
2021-11-18 12:40:29 -0500

    site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,40 @@
+import { Link, graphql } from "gatsby"
 import React from 'react'
 import Layout from '../components/Layout'
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
+import * as styles from '../styles/home.module.css'
+import self from '../images/thumbs/self.jpg'
 
-export default function About() {
+export default function About({data}) {
     return (
         <Layout>
         <div>
             <h1>About Me</h1>
-            <p>Hello! My name is Stephen and I enjoy creating things that make our lives better. My interest in development started back in 2012 when I decided to take an AP Computer Science course in high school!</p>
-            <p>Fast-forward to today, and I have had the privelage to study programming and work with the NSA as a student. My main focus these days is building predictive models with data.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+
+
+            <p>Hello! My name is Stephen and I am a data engineer who's goal it is to help make decisions that have positive and lasting impacts on people's lives</p>
+            <p>I enjoy spending my free time traveling, scuba diving, and surfing. If you can't find me traveling the world or in the ocean you will find me with friends and family or reading a book </p>
+            <p>A versatile individual with experience collecting, transforming, and organizing data for analysis to help make informed decisions. Excellent understanding and proficiency of platforms for effective data analyses, including SQL, spreadsheets, Tableau, and R. Strong communication, organizational, and analytical skills. Recently completed the Google Data Analytics Certificate- a rigorous, hands-on program that covers the entire scope of the data analysis process.</p>
+                <div>
+                <GatsbyImage image={getImage(data.file)} alt="self" />
+                </div>
+
         </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+  query self {
+    file(relativePath: { eq: "self.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(
+          layout: FULL_WIDTH
+          placeholder: BLURRED
+          formats: [AUTO, WEBP]
+        )
+      }
+    }
+  }
+  `
---
+++ src/projects/bellabeat.md
@@ -0,0 +1,29 @@
+---
+title: Bella Beat Case Study
+stack: Data Analytics
+slug: Data Analytics
+date: 2021-17-11
+thumb: ../images/thumbs/bellabeat.jpg
+featuredImg: ../images/thumbs/bellabeat.jpg
+---
+
+### **Data Analytics Project** 
+
+**What is it?** 
+
+My google analytics capstone focused on analyzing and visualizing data provided by 30 fit bit users in order to help the company make better decisions for their marketing campeigns.
+
+In this project I use machine learning and data engineering to interpret, analyze, and create recommendations based on the findings. Using the data provided I found that the activity level is the best indicator of how many calories a user will burn followed by a users distance traveled. I then foudn that time in bed and time spent in a sedentary state have little to no correlation.  
+
+**So what does this mean?**  
+Bellabeat can appeal to its users by creating in app programs that incentivize intense activity and distance traveled in order to meet fitness goals. Meanwhile, the app can help track time the user is in a sedentary state and beep every so often reminding encouraging the user to get up and move. 
+
+You can find the data and markdown file and full report [here](https://www.kaggle.com/stephenbroauds/bellabeat-case-study/notebook/ "Title"): \
+https://www.kaggle.com/stephenbroauds/bellabeat-case-study/notebook
+
+You can find the github file <span style="color:red"> [here](https://github.com/broadus20/fitbitData/ "Title") </span>: \
+<span style="color:red"> https://github.com/broadus20/fitbitData</span>
+
+
+You can find my presentation here: \
+https://docs.google.com/presentation/d/1fe-wyeCcmiOuf18OuHr0Z8bKfTlf4D_iMNgwqN4xU0I/edit?usp=sharing
